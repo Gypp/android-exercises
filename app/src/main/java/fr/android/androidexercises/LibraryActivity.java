018379bdbd8bbc5da77094bb040ce3e379c784e3
Gypp <gypp044@gmail.com>
2016-01-22 11:44:16 +0100

    first step with API (Timber, GSon)

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,10 +2,20 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 import timber.log.Timber;
 
 public class LibraryActivity extends AppCompatActivity {
+
+    public List<Book> books;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +26,39 @@ public class LibraryActivity extends AppCompatActivity {
         Timber.plant(new Timber.DebugTree());
 
         // TODO build Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://henri-potier.xebia.fr/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
         // TODO create a service
+        HenriPotierService henriPotierService = retrofit.create(HenriPotierService.class);
 
         // TODO listBooks()
+        Call<List<Book>> call = henriPotierService.listBooks();
+
+
 
         // TODO enqueue call and display book title
+        call.enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Response<List<Book>> response, Retrofit retrofit) {
+                books = response.body();
+                for(Book book : books){
+                    // TODO log books
+                    Timber.i(book.getTitle());
+                }
+            }
 
-        // TODO log books
+            @Override
+            public void onFailure(Throwable t) {
+                Timber.i("Fail");
+            }
+        });
+
 
         // TODO display book as a list
+        ListView listView = (ListView) findViewById(R.id.bookListView);
     }
 
 }
